12e23b53b14c6441be00ad442e6de1425264f65d
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2024-11-24 15:13:05 +0700

    Functionally Sign In / Sign Out Github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import { signOut } from "@/auth";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <>
-      <h1 className="h1-bold">Home</h1>
+      <h1 className="h1-bold">Welcome to Home</h1>
+
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
     </>
   );
 };
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,13 +1,37 @@
+"use client";
+
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { toast } from "@/hooks/use-toast";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constants/routes";
 
 const SocialAuth = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-[10px] px-4 py-3.5";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error),
+        toast({
+          title: "Sign - In Failed",
+          description:
+            error instanceof Error
+              ? error.message
+              : "An error occured during sign - in",
+          variant: "destructive",
+        });
+    }
+  };
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src={"/icons/github.svg"}
           width={20}
@@ -17,7 +41,7 @@ const SocialAuth = () => {
         />
       </Button>
 
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src={"/icons/google.svg"}
           width={20}
