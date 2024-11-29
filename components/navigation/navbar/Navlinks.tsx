56d54d32db7b0a9eddc5d2e10cc7c16ed67c51af
Rydhlnst <66582308+Rydhlnst@users.noreply.github.com>
2024-11-29 22:42:15 +0700

    Mobile Navigation // NavLinks

---
+++ components/navigation/navbar/Navlinks.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import ROUTES from "@/constants/routes";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Navlinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  // untuk membaca pathname makanya menggunakan client component
+  const pathname = usePathname();
+  const userId = 1;
+
+  return (
+    <>
+      {sidebarLinks.map((item) => {
+        // fungsi isActive untuk menentukan apakah item route sama dengan path name
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+
+        // Jika ditekan profile yang akan mengarah ke /profile akan menjadi dynamic route yang akan menampilkan id usernya
+        if (item.route === "/profile") {
+          if (userId) item.route = `${item.route}/${userId}`;
+          else return null;
+        }
+
+        const LinkComponent = (
+          <Link
+            href={item.route}
+            key={item.label}
+            // Classname disini berfungsi jika saat active NavListnya akan menampilkan warna yang berbeda
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light900"
+                : "text-dark-300_light900",
+              "flex items-center justify-start gap-4 bg-transparent p-4"
+            )}
+          >
+            <Image
+              width={20}
+              height={20}
+              src={item.imgURL}
+              alt={item.label}
+              className={cn({ "invert-colors": !isActive })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg-hidden"
+              )}
+            >
+              {item.label}
+            </p>
+          </Link>
+        );
+
+        return isMobileNav ? (
+          <SheetClose asChild key={item.route}>
+            {LinkComponent}
+          </SheetClose>
+        ) : (
+          <React.Fragment key={item.route}>{LinkComponent}</React.Fragment>
+        );
+      })}
+    </>
+  );
+};
+
+export default Navlinks;
